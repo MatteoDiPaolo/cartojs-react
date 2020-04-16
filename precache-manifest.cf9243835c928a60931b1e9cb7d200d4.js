bafaa2f6c9bf24fc09d66d3c27762cd7a6ca951f
matteo di paolantonio <matteodipaolantonio@matteos-MacBook-Pro.local>
2020-04-16 23:22:23 +0200

    chore: deploy app to github pages

---
+++ precache-manifest.cf9243835c928a60931b1e9cb7d200d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd26a46659c87a732b38bf7b85f14c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575c0c29e477504a566d",
+    "url": "/static/css/main.21a7cfea.chunk.css"
+  },
+  {
+    "revision": "b29b28522710fbd9da0e",
+    "url": "/static/js/2.2e47ff1c.chunk.js"
+  },
+  {
+    "revision": "5ad58d1cf78e5d8617e466d6de57090e",
+    "url": "/static/js/2.2e47ff1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575c0c29e477504a566d",
+    "url": "/static/js/main.4f438134.chunk.js"
+  },
+  {
+    "revision": "1e99f90b30ffe385e0af",
+    "url": "/static/js/runtime-main.005d0048.js"
+  }
+]);
